8175c5fd79c4ad97e1a134e89ba55504e7c6b8e3
Brendah <54269802+Brendah-jpg@users.noreply.github.com>
2020-05-18 14:48:00 +0300

    user with changes

--- user.php
+++ user.php
@@ -11,8 +11,8 @@
 			$this->first_name = $first_name;
 			$this->last_name = $last_name;
 			$this->city_name = $city_name;
+			
 		}
-
 		public function setUserId($user_id){
 			$this->user_id = $user_id;
 		}
@@ -20,31 +20,31 @@
 		public function getUserId(){
 			return $this-> $user_id;
 		}
-
 		public function save(){
-			
+
 			$servername = "localhost";
 			$username = "root";
 			$password = "";
 			$db = "ics3104";
-			
+
 			$conn = mysqli_connect($servername, $username, $password, $db);
-			
+
 			if(!$conn){
 				die("Connection failure");
-				}
+			}
+			
 			$fn = $this->first_name;
 			$ln = $this->last_name;
 			$city = $this->city_name;
-
-			$res = "INSERT INTO user (first_name, last_name, city_name) VALUES ('$fn', '$ln', '$city')";
+			
+			$res = "INSERT INTO user (first_name, last_name, user_city) VALUES ('$fn', '$ln', '$city')";
 
 			if(mysqli_query($conn, $res)){
-				return $res;
-				}
-			else{
-				echo "Error " . $res . "<br>" . mysqli_error($conn);
-			}
+				return $res; 
+					}
+			else {
+			 	echo "Error " . $res . "<br>" . mysqli_error($conn);
+				 }
 		}
 
 		public function readAll(){

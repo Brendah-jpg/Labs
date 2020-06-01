d1e7f55d4a0bea12319262b6d5706d2b4e60a779
Brendah <54269802+Brendah-jpg@users.noreply.github.com>
2020-06-01 16:11:29 +0300

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -1,31 +1,64 @@
-hp
+<?php
+	include_once 'DBConnector.php';
+	include_once 'User.php';
+	include_once 'FileUploader.php';
+	$conn = new DBConnector;
 
-include_once 'DBConnector.php';
-include_once 'user.php';
-$conn = new DBConnector;
+	if(isset($_POST['btn-save'])){
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$city = $_POST['city_name'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
 
-if(isset ($_POST['btn-save'])){
-	$first_name = $_POST['first_name'];
-	$last_name = $_POST['last_name'];
-	$city = $_POST['city_name'];
+		$user = new User($first_name, $last_name, $city, $username, $password);
+		$uploader = new FileUploader;
 
-	$user = new User($first_name, $last_name, $city);
-	$res = $user->save();
+		if(!$user->validateForm()){
+			$user->createFormErrorSessions();
+			header("Refresh: 0");
+			die();
+		}
 
-	if($res){
+	
+		$res = $user->save();
+		$file_upload_response = $uploader->uploadFile();
+
+	if($res && $file_upload_response){
 		echo "Save operation was successful";
 	} else{
 		echo "An error occurred!";
 	}
-}?>
+}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Home Page</title>
+		<script type="text/javascript" src="validate.js"></script>
+		<link rel="stylesheet" type = "text/css" href = "validate.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script type="text/javascript" src="timezone.js"></script>
 	</head>
 <body>
-	<form method="post">
+	<form method="post" name="user_details" id="user_details" onsubmit="return validateForm()" action="<?=$_SERVER['PHP_SELF']?>">
 		<table align="center">
+			<tr>
+				<td>
+					<div id = "form-errors">
+						<?php
+							session_start();
+							if(!empty($_SESSION['form_errors'])){
+								echo " ". $_SESSION['form_errors'];
+								unset($_SESSION['form_errors']);
+								}
+						?>
+				
+					</div>
+				</td>
+			</tr>
 			<tr>
 				<td><input type = "text" name = "first_name" required placeholder = "First Name"/></td>
 			</tr>
@@ -36,7 +69,19 @@ if(isset ($_POST['btn-save'])){
 				<td><input type = "text" name = "city_name" placeholder = "City"/></td>
 			</tr>
 			<tr>
+				<td><input type="text" name = "username" placeholder = "Username"/></td>
+			</tr>
+			<tr>
+				<td><input type="password" name="password" placeholder="Password"/></td>
+			</tr>
+			<tr>
+				<td>Profile Image: <input type="file" name="fileToUpload" id="fileToUpload"/></td>
+			</tr>
+			<tr>
 				<td><button type = "submit" name = "btn-save"><strong>SAVE</strong></button></td>
+			</tr>
+			<tr>
+				<td><a href = "login.php">Login</a></td>
 			</tr>
 		</table>
 	</form>

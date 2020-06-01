f8db5644bad83db4c65d0e65fbcaab735d078fa7
Brendah <54269802+Brendah-jpg@users.noreply.github.com>
2020-06-01 16:10:49 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,22 @@
 <?php
 	include_once 'DBConnector.php';
-	include_once 'user.php';
+	include_once 'User.php';
 
-	$con = new DBConnector;
+	$conn = new DBConnector;
 	if(isset($_POST['btn-login'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
+		//unsure if it works well
 		$instance = User::create();
 		$instance->setPassword($password);
 		$instance->setUsername($username);
 
 		if($instance->isPasswordCorrect()){
 			$instance->login();
-			$con->closeDatabase();
+			$conn->closeDatabase();
 			$instance->createUserSession();
 		}else{
-			$con->closeDatabase();
+			$conn->closeDatabase();
 			header("Location:login.php");
 		}
 	}
@@ -27,7 +28,7 @@
 	<link rel="stylesheet" type="text/css" href="validate.css">
 </head>
 <body>
-	<form method = "post" name="login" id="login" action="<?$_SERVER['PHP_SELF']?>">
+	<form method = "post" name="login" id="login" action="<?=$_SERVER['PHP_SELF']?>">
 		<table align = "center">
 			<tr>
 				<td><input type="text" name = "username" placeholder = "Username" required/></td>
